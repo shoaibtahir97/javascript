65b52da75c27aa415652cceb6f507d7a43630310
shoaibtahir97 <shoaibtahir97@gmail.com>
2021-04-14 17:12:49 +0500

    creted object file and done thill 220 page. Will complete the book after 1 Iftari

---
+++ 072.objects.js
@@ -0,0 +1,15 @@
+/*
+Suppose you have a web hosting provider that has different packages
+If you wanted to use its data in Javascript, you had to assigne each
+value to a different variable, which could be troublesome. 
+A better approach is to create object with properties.
+
+In the example present in book each hosting plan is a object with name
+plan1, plan2, plan3. 
+Each object has  5 properties name, price, space, transfer and pages
+In this case each object has a string property (name) and 
+four number properties(price, space, transfer and pages)
+
+Properties are just variables that are attached to an object.
+
+*/ 
